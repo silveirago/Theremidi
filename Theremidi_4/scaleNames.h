57adbce9a05fc1ef4b9c6c52f0399d063d67aabb
Gustavo Silveira <silveira.go@gmail.com>
2020-12-09 15:42:51 -0300

    bad code added

---
+++ Theremidi_4/scaleNames.h
@@ -0,0 +1,1 @@
+char* scaleNames[] = {"chromatic", "ionian", "dorian", "phrygian", "lydian", "mixolydiam", "aeolian", "locryan", "wholetone", "dim7", "octa 2-1", "octa 1-2", "major penta", "minor penta"};
